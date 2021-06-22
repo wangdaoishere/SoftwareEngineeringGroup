5795f0254e2ec74071d461a736a24a4395b0a764
GTHstar <50727917+GTHstar@users.noreply.github.com>
2021-06-22 23:36:28 +0800

    Add files via upload

---
+++ acos.h
@@ -0,0 +1,10 @@
+float aacos(float x){
+    unsigned char i;
+    float ans=x,t1=1,t2=x;x*=x;
+    for(i=3;i<51;i+=2){
+        t1*=(float)(i-2)/(float)(i-1);
+        t2*=x;
+        ans+=(t1*t2/(float)i);
+    }
+    return ans=1.5708-ans;
+}
