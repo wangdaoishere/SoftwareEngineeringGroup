d22943bb083a773820378875a1135e9b78190f9b
wangxin <870625197@qq.com>
2021-06-23 00:04:33 +0800

    v1.1 诱导公式的添加，限制输入在-π~π

--- mysin.py
+++ mysin.py
@@ -3,6 +3,11 @@ from utils import *
 
 
 def mySin(x):
+
+
+
+    y = 0
+    inductionFormula(x,y)     #诱导公式限制弧度制的输入为-π到π之间
     i=1                       #泰勒展开式项数标识位
     f=1                       #正负标识位
     s3=0                      #结果值
@@ -14,7 +19,7 @@ def mySin(x):
 
          g = 2*i-1            #项数的值
          b = myFactorial(g)   #阶乘结果
-         t = myPower(x,g)     #乘方结果
+         t = myPower(y,g)     #乘方结果
          s3 += t * f / b      #单项结果累加
          i += 1
          num += 1
@@ -24,3 +29,5 @@ def mySin(x):
 
     return s3
 
+x = 3* myPI
+print(mySin(x))
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 #本文件提供 阶乘函数 与 幂函数
 #阶乘函数
+myPI = 3.1415926535897932
+
 def myFactorial(a):
     sum=1
     for i in range(1,a+1):
@@ -15,3 +17,28 @@ def myPower(x,t):
        m *= x
        i += 1
     return m
+
+#诱导公式使定义域收敛
+def inductionFormula(x,y):
+
+    if x > myPI:
+
+        while True:
+
+            x -= (2 * myPI)
+
+            if x <= myPI:
+                y = x
+                break
+    elif x < (-1 * myPI):
+
+        while True:
+
+            x += (2 * myPI)
+
+            if x >= (-1 * myPI):
+                y = x
+                break
+    else: y = x
+
+    return y
