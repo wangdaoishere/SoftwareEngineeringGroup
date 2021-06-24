560452ec95e9de38d8586d93dedf19b10b2d29c7
GTHstar <853021665@qq.com>
2021-06-24 22:28:24 +0800

    atan函数计算没有正负的bug修正，以及弧度转换角度添加

--- atan.py
+++ atan.py
@@ -1,62 +1,52 @@
-from numpy import double
-
+import sys
 from utils import *
 from utils2 import *
 
+myPI = 3.1415926535897932
 
 
+def absxxxx(x):
+    if x >= 0 :
+        return x
+    else :
+        return -x
 
-def atan(x, inputflag,):
-
-        # x{-1,1}的计算
-        if ((x > -1) and (x < 1)):
-            # 声明变量总和，和每阶展开式数值
-            sum = x, x_pow = x, item = 0.00
-            # 定义正负与阶数
-            n = 1 ; fact = 1 ; sign = 1
-
-            while ((absxxxx(item)>0.000001) or (item == 0.00)):
-                fact = fact * (n+1) * (n+2)
-                x_pow *= x*x
-                sign = -sign
-                item = x_pow/ (n+2) *sign
-                sum += item
-                n += 2
-            return sum
-
-        #x为(1,)
-        elif (x >= 1):
-            sum = x + myPI/2
-            x_pow = x, item = 0.00
-            n = 1 ; sign = 1
-            while ((absxxxx(item) > 0.000001) or (item == 0.00)):
-                x_pow *= x*x
-                sign = -sign
-                item = -1 / (x_pow) * (n+2) *sign
-                sum += item
-                n +=2
-            return sum
-
-
-
-        #x为负
-        elif (x <= -1):
-            sum = x - myPI/2
-            x_pow = x. item = 0.00
-            n = 1
-            sign = 1
-            while ((absxxxx(item) > 0.000001) or (item == 0.00)):
-                x_pow *= x * x
-                sign = -sign
-                item = -1 / (x_pow) * (n+2) *sign
-                sum += item
-                n += 2
-            return sum
-#将X与-pi/2,pi/2相加返回的结果值精度在0.000001
-
-
-
-
-
-
-
+def atan(x):
+    mult = 0
+    sum = 0
+    xx = 0
+    sign = 1
+    if(x == 1):
+        sum = myPI/4
+    elif(x== -1):
+        sum = -myPI/4
+    elif((x>1)or(x<-1)):
+        mult = 1/x
+        sign = -sign
+        xx = mult * mult
+        for i in range(1, 300, 2):
+            sum += mult * sign / i
+            mult *=xx
+        if(x > 1): 
+            sum = sum+myPI/2
+        elif(x<-1):
+            sum = -(myPI/2 -sum)
+        else:
+            sum = sum
+    elif ((x > -1) or (x < 1)):
+        sum = x
+        x_pow = x
+        item = 1
+        # 定义正负与阶数
+        n = 1
+        fact = 1
+        sign = 1
+        while ((absxxxx(item)>0.000001) or (item == 0.00)):
+            fact = fact * (n+1) * (n+2)
+            x_pow *= x*x
+            sign = -sign
+            item = x_pow/ (n+2) *sign
+            sum += item
+            n += 2
+    return sum
+        
